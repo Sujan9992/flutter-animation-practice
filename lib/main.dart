445946993f7ea24194b7b6970677379f8c553bd7
Sujan9992 <suznthapa9992@gmail.com>
2023-04-13 19:21:52 +0545

    animated rotating container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,5 +12,57 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Animations',
         theme: ThemeData(primarySwatch: Colors.blue),
+        home: const AnimationScreen(),
+      );
+}
+
+class AnimationScreen extends StatefulWidget {
+  const AnimationScreen({super.key});
+
+  @override
+  State<AnimationScreen> createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 60));
+
+    _animation = Tween<double>(begin: 0, end: 2 * pi).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) => Transform(
+              alignment: Alignment.topCenter,
+              transform: Matrix4.identity()..rotateZ(_animation.value),
+              child: Container(
+                width: 5,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          ),
+        ),
       );
 }
