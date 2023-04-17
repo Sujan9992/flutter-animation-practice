d2e53aad6736681083bd04d69d50e01ce6cae532
Sujan9992 <suznthapa9992@gmail.com>
2023-04-17 09:25:35 +0545

    implicit animation animated container

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,57 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Animations',
         theme: ThemeData(primarySwatch: Colors.blue),
+        home: const AnimatedContainerAnimation(),
+      );
+}
+
+class AnimatedContainerAnimation extends StatefulWidget {
+  const AnimatedContainerAnimation({super.key});
+
+  @override
+  State<AnimatedContainerAnimation> createState() =>
+      _AnimatedContainerAnimationState();
+}
+
+const defaultWidth = 100.0;
+
+class _AnimatedContainerAnimationState
+    extends State<AnimatedContainerAnimation> {
+  var _isZoomedIn = false;
+  var _buttonText = 'Zoom In';
+  var _width = defaultWidth;
+  var _curve = Curves.bounceOut;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedContainer(
+                  width: _width,
+                  curve: _curve,
+                  duration: const Duration(milliseconds: 350),
+                  child: Image.asset('assets/images/image.jpg'),
+                ),
+              ],
+            ),
+            TextButton(
+              onPressed: () => setState(
+                () {
+                  _isZoomedIn = !_isZoomedIn;
+                  _buttonText = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                  _width = _isZoomedIn
+                      ? MediaQuery.of(context).size.width
+                      : defaultWidth;
+                  _curve = _isZoomedIn ? Curves.bounceIn : Curves.bounceOut;
+                },
+              ),
+              child: Text(_buttonText),
+            ),
+          ],
+        ),
       );
 }
